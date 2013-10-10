c5bcb61ec3bd44e0b7915828d37a6e87ad7d3690
pjholland <pjholland2000@yahoo.co.uk>
2013-10-10 13:36:20 +0100

    add remaining files that were missed at initial commit

---
+++ src/test/java/com/skrill/defects/first_test.java
@@ -0,0 +1,28 @@
+package com.skrill.defects;
+
+
+import com.skrill.pages.BasePage;
+import com.skrill.pages.*;
+import org.junit.After;
+import org.junit.Test;
+
+public class first_test  extends BasePage{
+
+    @After
+
+    public void ShutdownBrowser() {
+        getDriver().close();
+    }
+
+    @Test
+    public void first_test(){
+
+        ikeaHomePage home_page = new ikeaHomePage();
+
+        home_page.open();
+
+
+
+
+    }
+}
